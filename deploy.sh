67707d385e65d45ee9e4cae568f3515e1709789d
psumiya <sumiya.p@gmail.com>
2023-01-03 21:59:18 -0800

    Add deployment script

--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,18 @@ aws s3 sync . s3://$S3_BUCKET --dryrun \
 --exclude "*DS_Store" \
 --exclude 'drafts/*' \
 --exclude '.gitignore'
+
+read -p "Continue with deployment? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo "Uploading files to S3"
+
+  aws s3 sync . s3://$S3_BUCKET \
+  --exclude 'deploy.sh' \
+  --exclude '.*/*' \
+  --exclude './*' \
+  --exclude "*DS_Store" \
+  --exclude 'drafts/*' \
+  --exclude '.gitignore'
+fi
